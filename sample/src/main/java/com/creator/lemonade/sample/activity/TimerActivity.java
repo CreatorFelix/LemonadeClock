c132a84966e730e6aedda09ece047fd36881886f
Felix.Liang <lys74123@126.com>
2018-05-16 22:19:15 +0800

    Add a demo for the new timer widget

---
+++ sample/src/main/java/com/creator/lemonade/sample/activity/TimerActivity.java
@@ -0,0 +1,15 @@
+package com.creator.lemonade.sample.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.creator.lemonade.sample.R;
+
+public class TimerActivity extends BaseDemoActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer);
+    }
+}
