8195cd8ed9b8100c9d8d7a21953501f49be9ffcf
liangyusong <yusong.liang@gometech.com.cn>
2018-05-25 17:54:29 +0800

    Define a TimerState inner class to save states of timer

--- lemonadeclocklib/src/main/java/com/creator/lemonade/clock/util/TimerModel.java
+++ lemonadeclocklib/src/main/java/com/creator/lemonade/clock/util/TimerModel.java
@@ -1,4 +1,38 @@
 package com.creator.lemonade.clock.util;
 
+import android.os.SystemClock;
+
 public class TimerModel {
+
+    public static class TimerState {
+
+        /**
+         * A constant that used to define the default time
+         */
+        private static final int DEFAULT_TIME = 0;
+
+        /**
+         * Field that used to hold the elapsed real time at the start of timer.
+         *
+         * @see SystemClock#elapsedRealtime()
+         */
+        private long base = DEFAULT_TIME;
+
+        /**
+         * Field that used to hold the elapsed real time at the pause of timer.
+         *
+         * @see SystemClock#elapsedRealtime()
+         */
+        private long pause = DEFAULT_TIME;
+
+        /**
+         * Field that used to hold the total time of timer which set by user.
+         */
+        private long total = DEFAULT_TIME;
+
+        /**
+         * Field that indicates whether the timer has been started.
+         */
+        private boolean started;
+    }
 }
