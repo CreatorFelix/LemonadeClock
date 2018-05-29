d7d1d56ae780598b06fb362785d7c0f8e739a1c3
Felix.Liang <lys74123@126.com>
2018-05-29 10:10:02 +0800

    Refer the timer model in timer class

--- lemonadeclocklib/src/main/java/com/creator/lemonade/clock/widget/Timer.java
+++ lemonadeclocklib/src/main/java/com/creator/lemonade/clock/widget/Timer.java
@@ -5,8 +5,25 @@ import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 
 import com.creator.lemonade.clock.base.AbsClock;
+import com.creator.lemonade.clock.graphics.TimerDrawable;
+import com.creator.lemonade.clock.util.TimerModel;
 
 public class Timer extends AbsClock {
+
+    /**
+     * The logging tag used by this class with android.util.Log
+     */
+    private static final String LOG_TAG = "Lemonade_Timer";
+
+    /**
+     * A {@link TimerModel} controls the running of timer.
+     */
+    private final TimerModel mTimerModel;
+
+    /**
+     * A {@link TimerDrawable} defines the drawing of timer.
+     */
+    private final TimerDrawable mTimerDrawable;
 
     public Timer(Context context) {
         this(context, null);
@@ -14,5 +31,41 @@ public class Timer extends AbsClock {
 
     public Timer(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        mTimerDrawable = new TimerDrawable();
+        mTimerModel = new TimerModel();
+        mTimerModel.setTimerListener(new TimerModel.TimerWatcher() {
+            @Override
+            public void onTimeChanged(long restTime, long totalTime) {
+
+            }
+
+            @Override
+            public void onStateChanged(boolean started, boolean paused) {
+
+            }
+
+            @Override
+            public void onTimeout() {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        mTimerModel.attach(getHandler());
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        mTimerModel.detach();
+        super.onDetachedFromWindow();
+    }
+
+    @Override
+    protected void onWindowVisibilityChanged(int visibility) {
+        super.onWindowVisibilityChanged(visibility);
+        mTimerModel.setSuspend(visibility != VISIBLE);
     }
 }
