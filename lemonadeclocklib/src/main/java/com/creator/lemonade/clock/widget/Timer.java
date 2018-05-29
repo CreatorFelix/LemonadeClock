980a59e7fcc3f6c8846d3c5d54e74fef317ccd7b
Felix.Liang <lys74123@126.com>
2018-05-29 11:37:43 +0800

    Add a class to save the persistent state of timer

--- lemonadeclocklib/src/main/java/com/creator/lemonade/clock/widget/Timer.java
+++ lemonadeclocklib/src/main/java/com/creator/lemonade/clock/widget/Timer.java
@@ -1,8 +1,11 @@
 package com.creator.lemonade.clock.widget;
 
 import android.content.Context;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.AbsSavedState;
 
 import com.creator.lemonade.clock.base.AbsClock;
 import com.creator.lemonade.clock.graphics.TimerDrawable;
@@ -67,5 +70,70 @@ public class Timer extends AbsClock {
     protected void onWindowVisibilityChanged(int visibility) {
         super.onWindowVisibilityChanged(visibility);
         mTimerModel.setSuspend(visibility != VISIBLE);
+    }
+
+    @Nullable
+    @Override
+    protected Parcelable onSaveInstanceState() {
+        Parcelable superState = super.onSaveInstanceState();
+        SavedState ss = new SavedState(superState);
+        ss.timerState = mTimerModel.getState();
+        return ss;
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Parcelable state) {
+        if (!(state instanceof SavedState)) {
+            super.onRestoreInstanceState(state);
+            return;
+        }
+        SavedState ss = (SavedState) state;
+        mTimerModel.setState(ss.timerState);
+        super.onRestoreInstanceState(ss.getSuperState());
+    }
+
+    /**
+     * This is the persistent state that is saved by Timer.  Only needed
+     * if you are creating a subclass of Timer that must save its own
+     * state, in which case it should implement a subclass of this which
+     * contains that state.
+     */
+    public static class SavedState extends AbsSavedState {
+
+        public static final Creator<SavedState> CREATOR = new Parcelable.Creator<SavedState>() {
+            @Override
+            public SavedState createFromParcel(Parcel in) {
+                return new SavedState(in);
+            }
+
+            @Override
+            public SavedState[] newArray(int size) {
+                return new SavedState[size];
+            }
+        };
+
+        private TimerModel.TimerState timerState;
+
+        private SavedState(Parcelable superState) {
+            super(superState);
+        }
+
+        SavedState(Parcel in) {
+            super(in);
+            timerState = in.readParcelable(getClass().getClassLoader());
+        }
+
+        @Override
+        public void writeToParcel(Parcel dest, int flags) {
+            super.writeToParcel(dest, flags);
+            dest.writeParcelable(timerState, flags);
+        }
+
+        @Override
+        public String toString() {
+            return "SavedState{" +
+                    "timerState=" + timerState.toString() +
+                    '}';
+        }
     }
 }
