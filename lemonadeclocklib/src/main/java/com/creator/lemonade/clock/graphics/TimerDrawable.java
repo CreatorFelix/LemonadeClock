c7d682b8fbebc021f8380b0be231fad7a72d5664
Felix.Liang <lys74123@126.com>
2018-05-29 14:05:38 +0800

    Add some methods for timer drawable to set data

--- lemonadeclocklib/src/main/java/com/creator/lemonade/clock/graphics/TimerDrawable.java
+++ lemonadeclocklib/src/main/java/com/creator/lemonade/clock/graphics/TimerDrawable.java
@@ -9,6 +9,27 @@ import com.creator.lemonade.clock.base.AbsClockDrawable;
 
 public class TimerDrawable extends AbsClockDrawable {
 
+    private long mTotalTime;
+    private long mRestTime;
+
+    public TimerDrawable() {
+    }
+
+    public void setTotalTime(long totalTime) {
+        if (mTotalTime != totalTime) {
+            mTotalTime = totalTime;
+            invalidateSelf();
+        }
+    }
+
+    public void setRestTime(long restTime) {
+        restTime = Math.min(Math.max(0, restTime), mTotalTime);
+        if (mRestTime != restTime) {
+            mRestTime = restTime;
+            invalidateSelf();
+        }
+    }
+
     @Override
     public void draw(@NonNull Canvas canvas) {
 
